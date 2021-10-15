555c443527e502a883e3d0bd224f1795b5921808
GeetaSMath <geetasmath@gmail.com>
2021-10-14 20:35:33 -0600

    Added: SnakeLadder started with zero position

--- SnakeLadder_Prblm.java
+++ SnakeLadder_Prblm.java
@@ -1,6 +1,13 @@
+import java.util.Random;
+
 public class SnakeLadder_Prblm
-{
- public static void main(String[] args){
-  System.out.println("Welcome Note");
-}
+ {
+
+    public static void main(String[] args) {
+	    int pos = 0;
+        System.out.println("Welcome to Snake And Ladder program");
+        System.out.println("Single player at start position "+ pos);
+        
+    }
+    
 }

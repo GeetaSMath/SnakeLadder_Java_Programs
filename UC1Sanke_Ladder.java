7a7b8fce914da0490bb1eb6541b0eb88c9633322
GeetaSMath <geetasmath@gmail.com>
2021-10-16 12:24:24 -0600

    Added: Snake_Ladder at Position Roll

---
+++ UC1Sanke_Ladder.java
@@ -0,0 +1,19 @@
+
+import java.util.Random;
+
+public class UC1Sanke_Ladder {
+
+    public static void main(String[] args) {
+	    int pos = 0;
+        System.out.println("Welcome to Snake And Ladder program");
+        System.out.println("Single player at start position "+ pos);
+        RollDice();
+    }
+    public static void RollDice(){
+        Random random = new Random();
+        int dice = random.nextInt(6);
+        dice = dice+1;
+        System.out.println("Dice Num : "+ dice);
+
+    }
+}

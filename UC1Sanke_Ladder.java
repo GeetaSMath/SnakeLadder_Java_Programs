1cd8a577ae8973d3569bb2f4bdf87647d99bc04b
GeetaSMath <geetasmath@gmail.com>
2021-10-16 13:12:00 -0600

    Added: Players reaches winning at the psoition 100

--- UC1Sanke_Ladder.java
+++ UC1Sanke_Ladder.java
@@ -1,27 +1,31 @@
+
 import java.util.Random;
 
 public class UC1Sanke_Ladder {
 
     public static void main(String[] args) {
 	    int pos = 0;
-	    int dice = 0;
+	    int dice ;
         System.out.println("Welcome to Snake And Ladder program");
         System.out.println("Single player at start position "+ pos);
-        dice = rollDice();
-        mapTo(dice, pos);
+        while (pos <= 100){
+            dice = rollDice();
+            pos = mapTo(dice, pos);
+        }
+        System.out.println("Win the game pos is : "+ pos);
     }
     public static int rollDice(){
         Random random = new Random();
         int dice = random.nextInt(6);
         dice = dice+1;
-        System.out.println("Dice Num : "+ dice);
+        //System.out.println("Dice Num : "+ dice);
         return dice;
     }
-    public static void mapTo(int dice, int pos){
+    public static int mapTo(int dice, int pos){
         Random random = new Random();
         int map = random.nextInt(2);
         map = map +1;
-        System.out.println("Maping Method : "+ map);
+       System.out.println("Maping Method : "+ map);
 
         switch (map){
             case 1:
@@ -29,12 +33,16 @@ public class UC1Sanke_Ladder {
                 break;
             case 2:
                 pos = pos + dice;
-                System.out.println("your position after ladder is : "+ pos);
+               System.out.println("your position after ladder is : "+ pos);
                 break;
             case 3:
-                pos = pos - dice;
-                System.out.println("your position after snake is : "+ pos);
+                if (pos >= 0)
+                    pos = pos - dice;
+               System.out.println("your position after snake is : "+ pos);
                 break;
+            default:
+
         }
+        return pos;
     }
 }
